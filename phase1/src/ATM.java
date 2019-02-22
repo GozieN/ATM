91662780cfb40245fecfce27f6bf6c16cc54b991
j-chan-9108 <j.chan.9108@outlook.com>
2019-02-22 01:35:05 -0500

    added (incomplete) classes: Operator, BankManager, User. added a test class with psvm TestClass. changed [1stparentclass] name to ATM class. edited roles word doc for additional TODOs, some implementation notes, and clarity

---
+++ phase1/src/ATM.java
@@ -0,0 +1,18 @@
+package phase1;
+
+class ATM {
+    public int datemonth = 00;
+    public int dateday = 00;
+    public int dateyear = 0000;
+    public int timehour = 00;
+    public int timeminute = 00;
+    public int timesecond = 00;
+    public int num5bills = 0;
+    public int num10bills = 0;
+    public int num20bills = 0;
+    public int num50bills = 0;
+
+    ATM() {
+
+    }
+}
