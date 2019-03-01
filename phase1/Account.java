62491f2b952adf9e267d04e3c453741c8c5c491a
puriarj2 <arjsingh.puri@gmail.com>
2019-03-01 17:35:52 -0500

    Made account abstract.

--- phase1/Account.java
+++ phase1/Account.java
@@ -1,7 +1,15 @@
 package phase1;
 
-public class Account {
+public abstract class Account {
     //user can have multiple accounts
     private int accountNum;
     private String holderName;
+
+    public Account(int accountNum, String holderName){
+        this.accountNum = accountNum;
+        this.holderName = holderName;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
 }
