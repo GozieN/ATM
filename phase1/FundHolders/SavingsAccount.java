7bf4b6ef54c17cfe2a3b046e02e8cc3a68c5455b
maplethi <maple.thi@mail.utoronto.ca>
2019-03-10 14:51:07 -0400

    savings interest + helper method

--- phase1/FundHolders/SavingsAccount.java
+++ phase1/FundHolders/SavingsAccount.java
@@ -1,5 +1,14 @@
 package phase1.FundHolders;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Calendar;
 
 public class SavingsAccount extends Debit {
@@ -13,8 +22,24 @@ public class SavingsAccount extends Debit {
         super(accountNum, holderName, balance, "SavingsAccount");
     }
 
-//    use getdate method for interests
-//    store string of month and check if string is same as month in getdate method, if changed then increase interest
+    /**
+     * Helper function to get last line of date.txt file
+     * @return last line on file
+     */
+    public String getLastLine() {
+        String currLine;
+        String lastLine;
+
+        try {
+            BufferedReader br = new BufferedReader("./src/date.txt");
+
+            while ((currLine = br.readLine()) != null) {
+                lastLine = currLine;
+            }
+        } catch (IOException e) {
+        }
+        return lastLine;
+    }
 
     /**
      * Add monthly interest for savings account
@@ -22,11 +47,11 @@ public class SavingsAccount extends Debit {
      *                 user's savings account
      */
     public void monthlyInterest(double interest){
-//        String currMonth = getDate().substring(12,15)
-////        if (currMonth.equals(month in date)){
-//                setBalance(getBalance());
-//          } else {
-//            double increaseBy = interest * getBalance();
-//            setBalance(getBalance() - increaseBy);
+        LocalDateTime currdate = LocalDateTime.now();
+        if ((currdate.toString().substring(5,7)).equals(getLastLine().substring(2,4))){
+            setBalance(getBalance());
+        } else {
+            double increaseBy = interest * getBalance();
+            setBalance(getBalance() - increaseBy);
     }
 }
