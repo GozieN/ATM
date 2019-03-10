0cdcd9e095142db5ed9ebb5687faf4ebc8903093
Gozie Nwaka - nwakagoz <gozie.nwaka@mail.utronto.ca>
2019-03-10 00:49:38 -0500

    Undo transaction implementation

--- phase1/FundHolders/Account.java
+++ phase1/FundHolders/Account.java
@@ -37,10 +37,45 @@ public abstract class Account {
     }
     public String getAccountType() { return accountType; }
 
+    /**
+     * Get the history of the account's transactions
+     * @return The list of transaction history
+     */
+    public ArrayList<Transactions> getHistory(){
+        return history;
+    }
+
+    /**
+     *Get the history of the history of the account's transactions
+     * @return Set the of transaction history
+     */
+    public ArrayList<Transactions> setHistory(){
+        return history;
+    }
+
+    public void alterHistory(){
+        int lastElementIndex = history.size() - 1;
+        if (history.get(lastElementIndex).getLastAction().equals("bill")){
+            System.out.println("Sorry, your last action could not be reversed as you payed a bill");
+    }else{
+            if (history.get(lastElementIndex).getLastAction() == "transfer"){
+                history.get(lastElementIndex).depositToAccount(history.get(lastElementIndex).getLastAmount());
+                history.get(lastElementIndex).receiverBalanceAlterIncrease(history.get(lastElementIndex).getLastAmount());
+            } else if (history.get(lastElementIndex).getLastAction() == "withdraw"){
+                history.get(lastElementIndex).depositToAccount(history.get(lastElementIndex).getLastAmount());
+            }else if (history.get(lastElementIndex).getLastAction() == "deposit"){
+                history.get(lastElementIndex).withdrawFromAccount(history.get(lastElementIndex).getLastAmount());
+            }
+            history.remove(lastElementIndex);
+            System.out.println(getHolderName() + ", The last action that you performed a" + history.get(lastElementIndex).getLastAction()
+                    + " has been reversed upon your request.");
+        }
+    }
+
+
     public void updateHistory(Transactions transactionInfo){
         history.add(transactionInfo);
     }
-
 
     public void setBalance(double balance){
         this.balance = balance;
