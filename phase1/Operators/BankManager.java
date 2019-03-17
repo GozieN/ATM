1cdf62ab252160fea691d7bf1b267cef205906d5
Angela Chung <cangela25@gmail.com>
2019-03-17 16:23:19 -0400

    [Angela] changed file paths

--- phase1/Operators/BankManager.java
+++ phase1/Operators/BankManager.java
@@ -78,7 +78,7 @@ public class BankManager extends BankWorker implements Serializable{
         User newUser = new User(username, password);
 
         try {
-            String filename = "./phase1/Users.txt";
+            String filename = "phase2/Users.txt";
 
             FileOutputStream file = new FileOutputStream(filename);
             ObjectOutputStream out = new ObjectOutputStream(file);
