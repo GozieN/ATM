307586391eeab9572ec3a771d7a52847a19d8abe
maplethi <maple.thi@mail.utoronto.ca>
2019-03-23 21:14:00 -0400

    adding new account - prepaid credit

--- phase2/FundHolders/PrepaidCredit.java
+++ phase2/FundHolders/PrepaidCredit.java
@@ -3,7 +3,11 @@ package FundHolders;
 import Operators.*;
 import java.io.Serializable;
 
-public class PrepaidCredit extends Account implements Serializable {
+
+// PrepaidCredit account works like a prepaid gift card (only able to reload, balance never goes under 0, can only spend
+// up to limit given + incurs 3% deduction every month)
+
+public class PrepaidCredit extends Credit implements java.io.Serializable {
     private User user;
 
     public PrepaidCredit (User accountHolder, String accountType){ super(accountHolder, "Prepaid Credit"); }
