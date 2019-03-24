b300d19c05a71885958959a5a3735e1b6d049c4c
j-chan-9108 <j.chan.9108@outlook.com>
2019-03-24 14:01:32 -0400

    progress on GUI

--- phase2/UserInterface/RequestNewBankAccountCreationMenuController.java
+++ phase2/UserInterface/RequestNewBankAccountCreationMenuController.java
@@ -22,6 +22,8 @@ public class RequestNewBankAccountCreationMenuController extends Menu implements
 	@FXML
 	private ComboBox<String> bankAccountTypes;
 	@FXML
+	private Label bankAccountTypesStatus;
+	@FXML
 	private Label endStatus;
 
 	public void initialize(User user) {
@@ -30,20 +32,26 @@ public class RequestNewBankAccountCreationMenuController extends Menu implements
 	}
 
 	public void requestNewBankAccountCreation(ActionEvent event) throws Exception {
-		if (((String)this.bankAccountTypes.getValue()).equals("chequing")) {
-			GUI.getBM().createNewAccount(0, "chequing", this.user);
-			this.endStatus.setText("you have requested a new chequing bank account");
-		} else if (((String)this.bankAccountTypes.getValue()).equals("credit")) {
-			GUI.getBM().createNewAccount(0, "credit", this.user);
-			this.endStatus.setText("you have requested a new credit bank account");
-		} else if (((String)this.bankAccountTypes.getValue()).equals("debit")) {
-			GUI.getBM().createNewAccount(0, "debit", this.user);
-			this.endStatus.setText("you have requested a new debit bank account");
-		} else if (((String)this.bankAccountTypes.getValue()).equals("savings")) {
-			GUI.getBM().createNewAccount(0, "savings", this.user);
-			this.endStatus.setText("you have requested a new savings bank account");
+		if (!(this.bankAccountTypes.getSelectionModel().isEmpty())) {
+			if ((this.bankAccountTypes.getValue()).equals("chequing")) {
+				GUI.getBM().createNewAccount(0, "chequing", this.user);
+				this.bankAccountTypesStatus.setText("");
+				this.endStatus.setText("you have requested a new chequing bank account");
+			} else if ((this.bankAccountTypes.getValue()).equals("credit")) {
+				GUI.getBM().createNewAccount(0, "credit", this.user);
+				this.bankAccountTypesStatus.setText("");
+				this.endStatus.setText("you have requested a new credit bank account");
+			} else if ((this.bankAccountTypes.getValue()).equals("debit")) {
+				GUI.getBM().createNewAccount(0, "debit", this.user);
+				this.bankAccountTypesStatus.setText("");
+				this.endStatus.setText("you have requested a new debit bank account");
+			} else if ((this.bankAccountTypes.getValue()).equals("savings")) {
+				GUI.getBM().createNewAccount(0, "savings", this.user);
+				this.bankAccountTypesStatus.setText("");
+				this.endStatus.setText("you have requested a new savings bank account");
+			}
 		} else {
-			this.endStatus.setText("no bank account type selected. try again");
+			this.bankAccountTypesStatus.setText("no bank account type selected. try again");
 		}
 	}
 
---
+++ phase2/UserInterface/UserTransactionsMenuController.java
@@ -0,0 +1,113 @@
+package phase2.UserInterface;
+
+import phase2.FundHolders.*;
+import phase2.Operators.*;
+
+import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javafx.fxml.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.*;
+
+public class UserTransactionsMenuController extends Menu implements java.io.Serializable {
+	private User user;
+
+	@FXML
+	private Label withdrawStatus;
+	@FXML
+	private Label depositStatus;
+	@FXML
+	private Label transferStatus;
+	@FXML
+	private Label payBillStatus;
+
+	public void initialize(User user) {
+		this.user = user;
+	}
+
+	public void withdraw(ActionEvent event) throws Exception {
+		if (!(this.user.getAccountsCreated().isEmpty())) {
+			Stage mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("WithdrawMenuScene.fxml"));
+			Parent parent = loader.load();
+			Scene withdrawMenuScene = new Scene(parent);
+			UserInteractionsMenuController controller = loader.getController();
+			controller.initialize(this.user);
+			mainStage.setScene(withdrawMenuScene);
+			mainStage.show();
+		} else {
+			this.withdrawStatus.setText("you do not have any accounts to withdraw from");
+		}
+	}
+
+	public void deposit(ActionEvent event) throws Exception {
+		if (!(this.user.getAccountsCreated().isEmpty())) {
+			Stage mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("DepositMenuScene.fxml"));
+			Parent parent = loader.load();
+			Scene depositMenuScene = new Scene(parent);
+			UserInteractionsMenuController controller = loader.getController();
+			controller.initialize(this.user);
+			mainStage.setScene(depositMenuScene);
+			mainStage.show();
+		} else {
+			this.depositStatus.setText("you do not have any accounts to deposit to");
+		}
+	}
+
+	public void transfer(ActionEvent event) throws Exception {
+		if (!(this.user.getAccountsCreated().isEmpty())) {
+			Stage mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("TransferMenuScene.fxml"));
+			Parent parent = loader.load();
+			Scene transferMenuScene = new Scene(parent);
+			UserInteractionsMenuController controller = loader.getController();
+			controller.initialize(this.user);
+			mainStage.setScene(transferMenuScene);
+			mainStage.show();
+		} else {
+			this.transferStatus.setText("you do not have any accounts to transfer from");
+		}
+	}
+
+	public void payBill(ActionEvent event) throws Exception {
+		if (!(this.user.getAccountsCreated().isEmpty())) {
+			Stage mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("PayBillMenuScene.fxml"));
+			Parent parent = loader.load();
+			Scene payBillMenuScene = new Scene(parent);
+			UserInteractionsMenuController controller = loader.getController();
+			controller.initialize(this.user);
+			mainStage.setScene(payBillMenuScene);
+			mainStage.show();
+		} else {
+			this.payBillStatus.setText("you do not have any accounts to pay a bill from");
+		}
+	}
+
+	public void back(ActionEvent event) throws Exception {
+		Stage mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("UserInteractionsMenuScene.fxml"));
+		Parent parent = loader.load();
+		Scene userInteractionsMenuScene = new Scene(parent);
+		UserInteractionsMenuController controller = loader.getController();
+		controller.initialize(this.user);
+		mainStage.setScene(userInteractionsMenuScene);
+		mainStage.show();
+	}
+
+	public void exit(ActionEvent event) throws Exception {
+		super.exit(event);
+	}
+}
