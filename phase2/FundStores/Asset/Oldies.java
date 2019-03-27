f595d576e630431d71a1132b4b315c34705febe2
puriarj2 <arjsingh.puri@gmail.com>
2019-03-27 19:43:52 -0400

    oldies3

--- phase2/FundStores/Asset/Oldies.java
+++ phase2/FundStores/Asset/Oldies.java
@@ -3,8 +3,9 @@ package phase2.FundStores.Asset;
 import phase2.Operators.BankAccountUser.User;
 
 public class Oldies extends SavingsAccount implements java.io.Serializable {
-
+    private User nominee;
         public Oldies(User accountHolder, User nominee){
             super(accountHolder, nominee);
+            this.nominee = nominee;
         }
 }
