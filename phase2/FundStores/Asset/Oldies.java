f35bd7979d4e3b1f9ff40749c7fc557a4778ca20
j-chan-9108 <j.chan.9108@outlook.com>
2019-03-27 20:20:32 -0400

    Revert "oldies3"
    This reverts commit f595d576

--- phase2/FundStores/Asset/Oldies.java
+++ phase2/FundStores/Asset/Oldies.java
@@ -3,9 +3,8 @@ package phase2.FundStores.Asset;
 import phase2.Operators.BankAccountUser.User;
 
 public class Oldies extends SavingsAccount implements java.io.Serializable {
-    private User nominee;
+
         public Oldies(User accountHolder, User nominee){
             super(accountHolder, nominee);
-            this.nominee = nominee;
         }
 }
