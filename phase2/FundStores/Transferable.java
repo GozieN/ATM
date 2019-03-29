f73b3d56976c96e118d200a97145acf9072ffa2d
puriarj2 <arjsingh.puri@gmail.com>
2019-03-29 19:19:52 -0400

    Finished transferable interface.

--- phase2/FundStores/Transferable.java
+++ phase2/FundStores/Transferable.java
@@ -1,4 +1,7 @@
 package phase2.FundStores;
 
 public interface Transferable {
+    boolean withdrawFromAccount(double amount);
+    boolean withdrawFromATM(int amount);
+    boolean transfer(double amount, Account receiverAccount);
 }
