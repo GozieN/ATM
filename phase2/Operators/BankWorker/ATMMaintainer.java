6be0769b6eb60d292e533ab998352ce29a9dfdac
puriarj2 <arjsingh.puri@gmail.com>
2019-03-30 22:27:15 -0400

    Created ATM Maintainer.

---
+++ phase2/Operators/BankWorker/ATMMaintainer.java
@@ -0,0 +1,20 @@
+package phase2.Operators.BankWorker;
+
+public class ATMMaintainer extends Systems {
+
+    public void shutdownATM(){//shuts down the ATM
+
+    }
+
+    public void starupATM(){//starts ATM
+
+    }
+
+    public void InATMBills(){//
+
+    }
+
+    public void EmptyATMBills(){
+
+    }
+}
