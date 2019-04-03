927f6c7dcfac66fc455a46437696b82eb21a4d83
j-chan-9108 <j.chan.9108@outlook.com>
2019-04-02 23:43:40 -0400

    fixed is empty bug

--- phase2/UserInterface/RequestNewUserAccountCreationMenuController.java
+++ phase2/UserInterface/RequestNewUserAccountCreationMenuController.java
@@ -30,17 +30,49 @@ public class RequestNewUserAccountCreationMenuController extends Menu implements
 	@FXML
 	private Label newPasswordConfirmStatus;
 
-
-	@SuppressWarnings("unchecked")
 	public void requestNewUserAccountCreation (ActionEvent event) throws Exception {
-		for (User user : GUI.getBM().getUsers()) {
-			if (!(user.getUsername().equals(this.newUsernameIn.getText())) &&
-					!(this.newUsernameIn.getText().equals(""))) {
+		if (!(GUI.getBM().getUsers().isEmpty())) {
+			for (User user : GUI.getBM().getUsers()) {
+				if (!(user.getUsername().equals(this.newUsernameIn.getText())) &&
+						!(this.newUsernameIn.getText().equals(""))) {
+					this.newUsernameInStatus.setText("valid new username");
+				} else if (this.newUsernameIn.getText().equals("")) {
+					this.newUsernameInStatus.setText("this field cannot be left blank. try again");
+				} else {
+					this.newUsernameInStatus.setText("this username is not available. try again");
+				}
+				if (!(this.newPasswordIn.getText()).equals("")) {
+					this.newPasswordInStatus.setText("valid new password");
+				} else {
+					this.newPasswordInStatus.setText("this field cannot be left blank. try again");
+				}
+				if (!(this.newPasswordConfirmIn.getText().equals("")) &&
+						this.newPasswordConfirmIn.getText().equals(this.newPasswordIn.getText())) {
+					this.newPasswordConfirmStatus.setText("matches new password");
+				} else if (this.newPasswordConfirmIn.getText().equals("")) {
+					this.newPasswordConfirmStatus.setText("this field cannot be left blank. try again");
+				} else {
+					this.newPasswordConfirmStatus.setText("does not match new password. try again");
+				}
+				if (this.newUsernameInStatus.getText().equals("valid new username") &&
+						this.newPasswordInStatus.getText().equals("valid new password") &&
+						this.newPasswordConfirmStatus.getText().equals("matches new password")) {
+					Stage mainStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+					FXMLLoader loader = new FXMLLoader();
+					loader.setLocation(getClass().getResource("UserContractMenuScene.fxml"));
+					Parent parent = loader.load();
+					Scene userContractMenuScene = new Scene(parent);
+					UserContractMenuController controller = loader.getController();
+					controller.initialize(this.newUsernameIn.getText(), this.newPasswordIn.getText());
+					mainStage.setScene(userContractMenuScene);
+					mainStage.show();
+				}
+			}
+		} else {
+			if (!(this.newUsernameIn.getText().equals(""))) {
 				this.newUsernameInStatus.setText("valid new username");
 			} else if (this.newUsernameIn.getText().equals("")) {
 				this.newUsernameInStatus.setText("this field cannot be left blank. try again");
-			} else {
-				this.newUsernameInStatus.setText("this username is not available. try again");
 			}
 			if (!(this.newPasswordIn.getText()).equals("")) {
 				this.newPasswordInStatus.setText("valid new password");
